9e3b0c95c0200cd0da134aa6fed40c669eddc048
👩🏾‍💻 Nicoli Félix <nicoli@MacBook-Pro-de-.local>
2022-06-28 16:55:20 -0300

    :sparkles: page login

--- src/App.js
+++ src/App.js
@@ -48,6 +48,17 @@ function App() {
             </div>
           </div>
 
+          <div class=" my-2">
+            <div class="relative">
+              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+              </svg>
+            </div>
+            <input type="password" id="password" class="input w-64" placeholder="Senha" required/>
+            </div>
+          </div>
+
 
           
           <button type="button" class="mt-8 button bg-primaryColor text-white text-sm">Receber código</button>
